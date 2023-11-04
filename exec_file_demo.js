429fcbddaeff07f2947d84225cdc63a7d26ee42c
Your Name <you@example.com>
2023-11-04 20:44:13 +0200

    asdasda

--- exec_file_demo.js
+++ exec_file_demo.js
@@ -1,11 +1,5 @@
 const { execFileSync } = require('child_process');
 
-const exFile = execFileSync('git add .', (error, stdout, stderr) => {
-  if (error) {
-      console.error(`exec error: ${error}`);
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-}).toString().trim();
+const exFile = execFileSync('git',['add', '.']).toString().trim();
 
 console.dir(exFile)
