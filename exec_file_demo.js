1ab3244108b297087d5b3b54d9d41a9e85832733
Your Name <you@example.com>
2023-11-05 04:24:03 +0200

    adqwwwd

--- exec_file_demo.js
+++ exec_file_demo.js
@@ -3,3 +3,5 @@ const { execFileSync } = require('child_process');
 const exFile = execFileSync('git',['rev-parse', '--abbrev-ref', 'HEAD']).toString().trim();
 
 console.dir(exFile)
+
+throw new Error('asdasdsa')
