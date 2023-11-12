ca08ab5c0779501875dd2bbfbb55b55f800af1eb
Your Name <you@example.com>
2023-11-12 19:50:25 +0200

    asdasdsaaswqeq qwe qwssssadw as s

--- checkBranckName.js
+++ checkBranckName.js
@@ -1,10 +1,11 @@
 const child_process = require("child_process");
 
-const { PULL_TITLE, GITHUB_HEAD_REF, GITHUB_EVENT_NAME, GITHUB_REF_NAME } = process.env;
+const { PULL_TITLE, GITHUB_HEAD_REF, GITHUB_EVENT_NAME, GITHUB_REF_NAME, GITHUB_BASE_REF } = process.env;
 
 console.log("PULL_TITLE: ", PULL_TITLE);
 console.log("GITHUB_HEAD_REF: ", GITHUB_HEAD_REF);
 console.log("GITHUB_REF_NAME: ", GITHUB_REF_NAME);
+console.log("GITHUB_BASE_REF: ", GITHUB_BASE_REF);
 console.log("GITHUB_EVENT_NAME: ", GITHUB_EVENT_NAME);
 console.log("execFileSync: ",  child_process.execFileSync("git", ["rev-parse", "--abbrev-ref", "HEAD"]).toString().trim())
 console.log("=======================");
