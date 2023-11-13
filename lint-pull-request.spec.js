615da63fa74b8db9c994c26d97543cc66659a19c
Your Name <you@example.com>
2023-11-13 15:22:35 +0200

    asdada

--- lint-pull-request.spec.js
+++ lint-pull-request.spec.js
@@ -1,30 +1,37 @@
-import {executeScript, lintPullRequest} from "./lint-pull-request"
+const {executeScript, lintPullRequest} = require("./lint-pull-request");
 
 describe("lint-pull-request", () => {
   describe("lintPullRequest", () => {
     describe("should not return null", () => {
-      it("should not return null if pull request name doesn't start with [PILLP-$(TASK_NUMBER)]", () => {
+      it("should return an error message if pull request name doesn't start with [PILLP-$(TASK_NUMBER)]", () => {
         const pullRequestName = "[BILLB] project_deploy";
 
         const result = lintPullRequest(pullRequestName);
     
         expect(result).not.toBeNull();
       })
-      it("should not return null if there is no space between start of pull request name and body of pull request", () => {
+      it("should return an error message if there is no space between start of pull request name and body of pull request", () => {
         const pullRequestName = "[PILLP-1]project_deploy";
 
         const result = lintPullRequest(pullRequestName);
     
         expect(result).not.toBeNull();
       })
-      it("should not return null if there is more than one space between start of pull request name and body of pull request", () => {
+      it("should return an error message if there is more than one space between start of pull request name and body of pull request", () => {
         const pullRequestName = "[PILLP-1]   project_deploy";
 
         const result = lintPullRequest(pullRequestName);
     
         expect(result).not.toBeNull();
       })
-      it("should not return null if there is more than one space between start of pull request name and body of pull request", () => {
+      it("should return an error message if there if body of pull request name doesn't in snake_case", () => {
+        const pullRequestName = "[PILLP-1] ProjectDeploy";
+
+        const result = lintPullRequest(pullRequestName);
+    
+        expect(result).not.toBeNull();
+      })
+      it("should return null for valid name of pull request", () => {
         const pullRequestName = "[PILLP-1] ProjectDeploy";
 
         const result = lintPullRequest(pullRequestName);
